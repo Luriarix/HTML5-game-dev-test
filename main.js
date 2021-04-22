bc8470ca6f89bd304c69fd146ad5ed58a40e08bd
Andrei L Ionita <ionitaandrei_lucian@yahoo.ro>
2021-04-22 19:21:25 +0100

    trying to do the game logic

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-var Safe = /** @class */ (function () {
-    function Safe(safeNumber) {
-        this.open = false;
-        this.id = safeNumber;
-    }
-    return Safe;
-}());
-var safes;
 function settingBackground() {
     var canvas = document.getElementById('application'), context = canvas.getContext('2d');
     var image = new Image();
@@ -13,13 +5,41 @@ function settingBackground() {
     image.onload = function () {
         context.drawImage(image, 1, 1, 298, 148);
     };
-    for (var index = 0; index < 9; index++) {
-        safes[index] = new Safe[index];
-    }
 }
-function settingSafes() {
-    // var code = document.getElementById('code').innerHTML;
-    console.log(String(safes[2].id));
+function getNumber() {
+    var luck = Math.floor(Math.random() * 9);
+    return luck;
+}
+function settingSafes(lucky) {
+    var code = document.getElementById('code').innerText;
+    var luck = lucky + 1;
+    for (var index = 1; index < 10; index++) {
+        if (index == luck) {
+            var safe = document.getElementById('safe' + String(index));
+            var safe_check = Number(safe.innerText);
+            if (safe_check == luck) {
+                console.log('Lucky U!');
+                var toggled = false;
+                var temp = '';
+                for (var index_1 = 0; index_1 < code.length; index_1++) {
+                    var temp2 = code[index_1];
+                    if (temp2 == '-' && toggled == false) {
+                        temp += String(luck);
+                        toggled = true;
+                    }
+                    else {
+                        temp += temp2;
+                    }
+                }
+                document.getElementById('code').innerText = temp;
+            }
+            else {
+                luck = getNumber();
+            }
+        }
+    }
+    // code = '';
+    console.log(luck);
     // var canvas = <HTMLCanvasElement> document.getElementById('application'),
     // context = canvas.getContext('2d');
     // context.font = "40px Titan";
--- main.ts
+++ main.ts
@@ -1,13 +1,3 @@
-class Safe {
-  id: number;
-  open: boolean = false;
-  constructor(safeNumber: number) {
-    this.id = safeNumber;
-  }
-}
-
-var safes: Safe[];
-
 function settingBackground() {
   var canvas = <HTMLCanvasElement>document.getElementById('application'),
     context = canvas.getContext('2d');
@@ -17,20 +7,58 @@ function settingBackground() {
   image.onload = function () {
     context.drawImage(image, 1, 1, 298, 148);
   }
-  for (let index = 0; index < 9; index++) {
-    safes[index] = new Safe[index];
+}
+
+function getNumber() {
+  var luck: number = Math.floor(Math.random() * 9);
+  return luck;
+}
+
+function showCode(luck : number) {
+  var code = document.getElementById('code').innerText;
+  var toggled : boolean = false;
+  var temp: string = '';
+
+  for (let index = 0; index < code.length; index++) {
+    var temp2 = code[index];
+    if (temp2 == '-' && toggled == false) {
+      temp += String(luck);
+      toggled = true;
+    }
+    else {
+      temp += temp2;
+    }
   }
+
+  document.getElementById('code').innerText = temp;
 }
 
 
 
-function settingSafes() {
-  // var code = document.getElementById('code').innerHTML;
-  console.log(String(safes[2].id));
+function settingSafes(lucky : number) {
+
+  var luck = lucky + 1;
+
+  for (let index = 1; index < 10; index++) {
+    if (index == luck) {
+      var safe = document.getElementById('safe' + String(index));
+      var safe_check = Number(safe.innerText);
+
+      if (safe_check == luck) {
+        showCode(luck)
+      }
+      else {
+        luck = getNumber();
+      }
+    }
+
+  }
+
+  // code = '';
+  console.log(luck);
   // var canvas = <HTMLCanvasElement> document.getElementById('application'),
   // context = canvas.getContext('2d');
   // context.font = "40px Titan";
   // context.textBaseline = 'top';
-
   // context.fillText("1 - - -", 355, 110);
   }
